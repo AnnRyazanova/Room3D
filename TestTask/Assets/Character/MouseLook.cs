2e8a8c61bd155668ef144526f369b486233ee437
Анна Рязанова <ryazanoanya@yandex.ru>
2017-11-26 15:52:36 +0300

    Add player and movements

---
+++ TestTask/Assets/Character/MouseLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    
+    // Чувствительность мыши
+    public float sensitivity = 3.0f;
+
+    // Максимальный и минимальный углы, на которые можно смотреть по вертикали
+    private float minAngle = -10;
+    private float maxAngle = 10;
+
+    private Rigidbody body;
+    private float rotationX;
+
+    void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+
+        // отключение влияния среды на игрока
+        if (body != null)
+            body.freezeRotation = true;
+    }
+
+
+    void Update()
+    {
+        // Вертикальная ось - Mouse Y
+        // Горизонтальная ось - Mouse X
+        var xRotation = Input.GetAxis("Mouse Y") * sensitivity;
+        var yRotation = Input.GetAxis("Mouse X") * sensitivity;
+
+        // управление головой (камерой) по движениям мыши
+        float rotationY = transform.localEulerAngles.y + yRotation;
+        rotationX += xRotation;
+        rotationX = Mathf.Clamp(rotationX, minAngle, maxAngle);
+        transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+    }
+
+
+}
---
+++ TestTask/Assets/Character/PlayerBehavior.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    // Чувствительность мыши
+    public float sensitivity = 3.0f;
+    // Скорость игрока
+    public float speed = 3.0f;
+
+    // Максимальный и минимальный углы, на которые можно смотреть по вертикали
+    private float minAngle = -10;
+    private float maxAngle = 10;
+
+    private Rigidbody player;
+    private float rotationX;
+
+    private CharacterController characterController;
+    
+
+    void Start()
+    {
+
+        characterController = GetComponent<CharacterController>();
+        player = GetComponent<Rigidbody>();
+
+        // отключение влияния среды на игрока
+        if (player != null)
+            player.freezeRotation = true;
+    }
+
+    // управление "головой" (камерой) по движениям мыши
+    private void MouseMove()
+    {
+        // Вертикальная ось - Mouse Y
+        // Горизонтальная ось - Mouse X
+        var xRotation = Input.GetAxis("Mouse Y") * sensitivity;
+        var yRotation = Input.GetAxis("Mouse X") * sensitivity;
+
+        // управление головой (камерой) по движениям мыши
+        float rotationY = transform.localEulerAngles.y + yRotation;
+        rotationX += xRotation;
+        rotationX = Mathf.Clamp(rotationX, minAngle, maxAngle);
+        transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+    }
+
+    // Управление игроком с помощью клавиатуры
+    private void MovePlayer()
+    {
+        var horizontalMovements = Input.GetAxis("Horizontal") * speed;
+        var verticalMovements = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(horizontalMovements, 0, verticalMovements);
+        // Ограничение на скорость движения по диагонали
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement *= Time.deltaTime;
+        // Игрок не будет ходить сквозь пол, стены и другие объекты
+        characterController.Move(transform.TransformDirection(movement));
+    }
+
+    void Update()
+    {
+        MouseMove();
+        MovePlayer();
+    }
+
+
+}
